0a0525dc016c0aeaa6c69a5f1d3934e708922a99
emily.lira <emily_saraiva@hotmail.com.br>
2020-08-20 13:01:07 -0300

    primeiro commit, onde foi ciada os arquivos htaccess, index, primeira classe rota, primeiro metodo e checando se o controlador existe

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+include './../app/libraries/Rota.php'
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio Mirium</title>
+</head>
+
+<body>
+    <?php
+        $rotas = new Rota();
+    ?>
+
+</body>
+
+</html>
